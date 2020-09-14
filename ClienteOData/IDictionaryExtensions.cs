53956b4840920f1df1e127a14257aefc72108f40
Luciano Pucciarelli <luciano.lcc@gmail.com>
2020-09-14 10:59:28 -0500

    Se agrega ejemplo de POST

---
+++ ClienteOData/IDictionaryExtensions.cs
@@ -0,0 +1,57 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+
+namespace ClienteOData
+{
+    public static class IDictionaryExtensions
+    {
+        /// <summary>
+        /// Fuente: https://gunnarpeipman.com/serialize-url-encoded-form-data/
+        /// </summary>
+        public static IDictionary<string, string> ToKeyValue(this object metaToken)
+        {
+            if (metaToken == null)
+            {
+                return null;
+            }
+
+            var serializer = new JsonSerializer() { ReferenceLoopHandling = ReferenceLoopHandling.Ignore };
+            JToken token = metaToken as JToken;
+            if (token == null)
+            {
+                return ToKeyValue(JObject.FromObject(metaToken, serializer));
+            }
+
+            if (token.HasValues)
+            {
+                var contentData = new Dictionary<string, string>();
+                foreach (var child in token.Children().ToList())
+                {
+                    var childContent = child.ToKeyValue();
+                    if (childContent != null)
+                    {
+                        contentData = contentData.Concat(childContent)
+                                                 .ToDictionary(k => k.Key, v => v.Value);
+                    }
+                }
+
+                return contentData;
+            }
+
+            var jValue = token as JValue;
+            if (jValue?.Value == null)
+            {
+                return null;
+            }
+
+            var value = jValue?.Type == JTokenType.Date ?
+                            jValue?.ToString("o", CultureInfo.InvariantCulture) :
+                            jValue?.ToString(CultureInfo.InvariantCulture);
+
+            return new Dictionary<string, string> { { token.Path, value } };
+        }
+    }
+}
--- ClienteOData/Program.cs
+++ ClienteOData/Program.cs
@@ -15,8 +15,9 @@ namespace ClienteOData
 
         static void Main(string[] args)
         {
-            
-            Console.WriteLine("Haciendo llamada...");
+
+            ////////////// GET ////////////// 
+            Console.WriteLine("Haciendo llamada GET...");
             Console.WriteLine("http://localhost:5000/odata/personas?$expand=telefono,direccion&$filter=Id eq 1");
 
             var task = Task.Run(async () =>
@@ -24,6 +25,29 @@ namespace ClienteOData
                 var personas = await GetPersonas();
                 Console.WriteLine(JsonConvert.SerializeObject(personas, Formatting.Indented));
             });
+            task.Wait();
+
+            ////////////// POST ////////////// 
+            Console.WriteLine("Haciendo llamada POST...");
+            task = Task.Run(async () =>
+            {
+                await GuardarPersona(new Persona()
+                {
+                    NombreYApellido = "Juan Perez",
+                    Edad = 23,
+                    Telefono = new List<Telefono>()
+                    {
+                        new Telefono() { TelefonoDesc = "+528885555444" },
+                        new Telefono() { TelefonoDesc = "+551234567890" }
+                    },
+                    Direccion = new List<Direccion>()
+                    {
+                        new Direccion() { DireccionDesc = "Calle 23" },
+                        new Direccion() { DireccionDesc = "Avenida 28 #43 depto. 4" }
+                    }
+                });
+            });
+            task.Wait();
 
             Console.ReadLine();
         }
@@ -55,8 +79,13 @@ namespace ClienteOData
                     throw new Exception(error);
                 }
 
-                var respString = await respGET.Content.ReadAsStringAsync();
-                var personas = JsonConvert.DeserializeObject<PersonaResponse>(respString);
+                //var respString = await respGET.Content.ReadAsStringAsync();
+                //var personas = JsonConvert.DeserializeObject<PersonaResponse>(respString);
+
+                // The lines above can be replaced with this using
+                // https://www.nuget.org/packages/Microsoft.AspNet.WebApi.Client/5.2.7
+                // Microsoft.AspNet.WebApi.Client
+                var personas = await respGET.Content.ReadAsAsync<PersonaResponse>();
 
                 return personas.value;
             }
@@ -64,6 +93,33 @@ namespace ClienteOData
             {
                 throw ex;
             }
+        }
+
+        private static async Task GuardarPersona(Persona persona)
+        {
+            HttpClient client = new HttpClient
+            {
+                BaseAddress = new Uri(URLBase)
+            };
+            var uriBuilder = new UriBuilder(URLBase)
+            {
+                Path = "odata/personas"
+            };
+
+            IEnumerable<KeyValuePair<string, string>> parameters = new Dictionary<string, string>();
+            parameters = parameters.Concat(persona.ToKeyValue());
+            FormUrlEncodedContent content = new FormUrlEncodedContent(parameters);
+
+            HttpResponseMessage responsePOST = await client.PostAsync(uriBuilder.ToString(), content);
+            if (!responsePOST.IsSuccessStatusCode)
+            {
+                string error = "ERROR! " + responsePOST.StatusCode;
+                throw new Exception(error);
+            }
+
+            var resp = await responsePOST.Content.ReadAsStringAsync();
+
+            Console.WriteLine("Respuesta " + Environment.NewLine + resp);
         }
     }
 }
